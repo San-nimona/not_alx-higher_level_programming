bef897da1e0d152b0d384e4fa339424f21e25692
San-nimona <kaybryanmc@gmail.com>
2023-06-08 15:39:17 -0700

    hid

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+
     import hidden_4
+
     names = dir(hidden_4)
-    for i in names:
-        if i[0] == "_" and i[1] == "_":
-            continue
-        print(i)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
